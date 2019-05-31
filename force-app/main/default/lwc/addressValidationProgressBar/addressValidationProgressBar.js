960a266680a702b7b15f8d7723e9633c7db0eace
Kai Tribble <Kai.Tribble@ef.com>
2019-05-30 19:02:00 -0700

    Create the address validation progress bar lightning component

--- force-app/main/default/lwc/addressValidationProgressBar/addressValidationProgressBar.js
+++ force-app/main/default/lwc/addressValidationProgressBar/addressValidationProgressBar.js
@@ -1,3 +1,12 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class AddressValidationProgressBar extends LightningElement {}
+
+export default class AddressValidationProgressBar extends LightningElement {
+
+    @track 
+    addressStatus = 'Pending';
+    // set value based on account address status value
+
+    // add has error if account validation is invalid
+    
+}
